510a6e1df8fea99f0d8e4ecbb9c0d9d02b07bb48
Aleks Nikolov <tcr.official.world@gmail.com>
2022-08-13 18:50:55 +0300

    gotta work on script, now going to village

---
+++ tips_calculator/main/script.js
@@ -0,0 +1,7 @@
+const bill = document.querySelector('.bill-input');
+const tip = document.querySelector('.btns-container');
+const people = document.querySelector('.people-input');
+
+function results(){
+
+}
